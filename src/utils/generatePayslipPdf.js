9ab486cfe143b2ea019202d6fffba4293aa3dc7e
Tehnikrangrej <irshadrangreh@gmail.com>
2026-03-10 11:54:54 +0530

    fix

--- src/utils/generatePayslipPdf.js
+++ src/utils/generatePayslipPdf.js
@@ -1,36 +1,23 @@
 const puppeteer = require("puppeteer");
-const payslipTemplate = require("../templates/payslipTemplate");
+const template = require("../templates/payslipTemplate");
 
 module.exports = async (payslip, settings) => {
 
+  const html = template(payslip, settings);
+
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
 
   const page = await browser.newPage();
 
-  const html = payslipTemplate(payslip, settings);
-
-  //////////////////////////////////////////////////////
-  // IMPORTANT: wait for images to load
-  //////////////////////////////////////////////////////
   await page.setContent(html, {
-    waitUntil: "networkidle0"
+    waitUntil: "networkidle0",
   });
-
-  //////////////////////////////////////////////////////
-  // wait for logo image
-  //////////////////////////////////////////////////////
-  if (settings?.companyLogo) {
-    try {
-      await page.waitForSelector("img");
-    } catch (e) {}
-  }
 
   const pdfBuffer = await page.pdf({
     format: "A4",
-    printBackground: true
+    printBackground: true,
   });
 
   await browser.close();
